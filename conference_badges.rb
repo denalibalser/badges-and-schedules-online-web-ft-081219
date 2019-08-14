1e311cd5fa05ce66d6e85be8279f332f23f7c680
Denali Balser <denali.balser167@gmail.com>
2019-08-14 17:56:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,5 @@
-def badge_maker(array)
-  arr = []
-  array.each do |name|
-    arr << "Hello, my name is #{name}."
-  end
-  arr
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
