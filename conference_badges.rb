80a9da691963bc1417adfd6fc408f47656691d5a
Denali Balser <denali.balser167@gmail.com>
2019-08-14 18:03:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,13 @@ def batch_badge_creator(array)
   end
   arr
 end
+
+def assign_rooms(array)
+  arr = []
+  rooms = 1
+  array.each do |name, room|
+    arr << "Hello #{name}! You'll be assigned to room #{room}!"
+    rooms += 1
+  end
+  arr
+end
